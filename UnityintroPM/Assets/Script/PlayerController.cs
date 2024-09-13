4819e933645739f9109021d909eb50efe8055477
Havl6396 <fanpan967@gmail.com>
2024-09-13 13:10:58 -0700

    Added a new Player Controller Script, made an obstacle course, need to learn how to make a killbarrier that restarts the level, enemies and attacks

--- UnityintroPM/Assets/Script/PlayerController.cs
+++ UnityintroPM/Assets/Script/PlayerController.cs
@@ -4,71 +4,93 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody myRB;
-    private Camera PlayerCam;
-    public float speed = 1.0f;
-    
-    public float jumpheight = 1.0f;
-    private Vector2 camerarotation;
-    public float sprintmult = 2.5f;
-    public float mousesensitivity = 1.0f;
-    public float xsensitivity = 1.0f;
-    public float ysensitivity = 1.0f;
-    public float camerarotationlimit = 100f;
-    public bool sprintmode = false;
+    Rigidbody myRB;
+    Camera playerCam;
+
+    Vector2 camRotation;
+
+    public bool sprintMode = false;
+    public bool dashMode = false;
+
+    [Header("Movement Settings")]
+    public float speed = 10.0f;
+    public float dashmult = 10.0f;
+    public float sprintMultiplier = 2.5f;
+    public float jumpHeight = 5.0f;
+    public float groundDetectDistance = 1f;
+
+    [Header("User Settings")]
+    public bool sprintToggleOption = false;
+    public float mouseSensitivity = 2.0f;
+    public float Xsensitivity = 2.0f;
+    public float Ysensitivity = 2.0f;
+    public float camRotationLimit = 90f;
 
     // Start is called before the first frame update
     void Start()
     {
         myRB = GetComponent<Rigidbody>();
-        PlayerCam = transform.GetChild(0).GetComponent<Camera>();
+        playerCam = transform.GetChild(0).GetComponent<Camera>();
+
+        camRotation = Vector2.zero;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
+        camRotation.x += Input.GetAxisRaw("Mouse X") * mouseSensitivity;
+        camRotation.y += Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
+
+        camRotation.y = Mathf.Clamp(camRotation.y, -camRotationLimit, camRotationLimit);
+
+        playerCam.transform.localRotation = Quaternion.AngleAxis(camRotation.y, Vector3.left);
+        transform.localRotation = Quaternion.AngleAxis(camRotation.x, Vector3.up);
+
         Vector3 temp = myRB.velocity;
 
+        float verticalMove = Input.GetAxisRaw("Vertical");
+        float horizontalMove = Input.GetAxisRaw("Horizontal");
 
-        temp.x = Input.GetAxisRaw("Vertical") * speed;
-        temp.z = Input.GetAxisRaw("Horizontal") * speed;
-
-        if (Input.GetKeyDown(KeyCode.Space) && Physics.Raycast(transform.position, -transform.up, 2.2f))
+        if (!sprintToggleOption)
         {
-            temp.y = jumpheight;
+            if (Input.GetKey(KeyCode.LeftShift))
+                sprintMode = true;
+
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+                sprintMode = false;
         }
 
-        myRB.velocity = temp.x * transform.forward + (temp.z * transform.right) + (temp.y * transform.up);
-
-        if (Input.GetKey(KeyCode.LeftShift))
+        if (sprintToggleOption)
         {
-            sprintmode = true;
+            if (Input.GetKey(KeyCode.LeftShift) && verticalMove > 0)
+                sprintMode = true;
+
+            if (verticalMove <= 0)
+                sprintMode = false;
         }
 
-        if (sprintmode)
-        {
-            temp.x = Input.GetAxisRaw("Vertical") * speed * sprintmult;
-        }
+        if (!sprintMode)
+            temp.x = verticalMove * speed;
 
-        if (!sprintmode)
-        {
-            temp.x = Input.GetAxisRaw("Vertical") * speed;
-        }
+        if (sprintMode)
+            temp.x = verticalMove * speed * sprintMultiplier;
 
-        if (Input.GetKeyUp(KeyCode.LeftShift))
-        {
-            sprintmode = false;
-        }
+        temp.z = horizontalMove * speed;
 
-        if()
+        if (Input.GetKeyDown(KeyCode.Mouse2) && !dashMode)
+            dashMode = true;
 
-        camerarotation.x += Input.GetAxisRaw("Mouse X") * mousesensitivity;
-        
-        camerarotation.y += Input.GetAxisRaw("Mouse Y") * mousesensitivity;
+        if (dashMode)
+            temp.x = verticalMove * speed * dashmult;
 
-        camerarotation.y = Mathf.Clamp(camerarotation.y, -camerarotationlimit, camerarotationlimit);
+        if (dashMode)
+            dashMode = false;
 
-        PlayerCam.transform.localRotation = Quaternion.AngleAxis(camerarotation.y, Vector3.left);
-        transform.localRotation = Quaternion.AngleAxis(camerarotation.x, Vector3.up);
+        if (Input.GetKeyDown(KeyCode.Space) && Physics.Raycast(transform.position, -2*transform.up, groundDetectDistance))
+            temp.y = jumpHeight;
+
+        myRB.velocity = (temp.x * transform.forward) + (temp.z * transform.right) + (temp.y * transform.up);
     }
 }
